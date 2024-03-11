1c10db0e358b19e401be4b92e8a72d31d8da85d0
Drew Bruck <drewtbruck@gmail.com>
2024-03-11 02:03:02 -0400

    modified chat page, added .dart files for all pages, began creating profile page

--- cuny_connect/lib/main.dart
+++ cuny_connect/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'chat_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,40 +16,44 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.purple,
         primaryColor: const Color.fromARGB(255, 63, 4, 73),
       ),
-      home: const ChatPage(),
+      home: const HomePage(),
     );
   }
 }
 
-class ChatPage extends StatefulWidget {
-  const ChatPage({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  @override
-  State<ChatPage> createState() => _ChatPageState();
-}
-
-class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //this section is for the top purple bar and its icons
       appBar: AppBar(
-        leading: const Icon(Icons.add_circle, color: Colors.white),
+        leading: const Icon(Icons.add_comment_outlined, color: Colors.white),
         title:
             const Text('CUNY Connect', style: TextStyle(color: Colors.white)),
         actions: [
+          //search icon
           IconButton(
-              onPressed: () => {},
-              icon: const Icon(Icons.search),
-              color: Colors.white),
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+            color: Colors.white,
+          ),
+
+          //profile icon
           IconButton(
-              onPressed: () => {},
-              icon: const Icon(Icons.person_outlined),
-              color: Colors.white),
+            onPressed: () {},
+            icon: const Icon(Icons.person_outlined),
+            color: Colors.white,
+          ),
         ],
         centerTitle: true,
-        backgroundColor:
-            Theme.of(context).primaryColor, // Explicitly set background color
+        backgroundColor: Theme.of(context)
+            .primaryColor, // had to explicitly set background color
       ),
+      //body: const ChatPage() is where we target our chats/ socket.io/firebase
+      body: const ChatPage(),
+      //the following chunk is for the bottom purple bar and its icons.
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Theme.of(context).primaryColor,
         selectedItemColor: Colors.white,
